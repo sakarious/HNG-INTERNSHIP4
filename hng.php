25840087d7cc7421d0a4f0b74d926b674f88adc7
sakarious <ajayimooseize@gmail.com>
2018-04-21 17:45:25 +0100

    first commit

---
+++ hng.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<style>
+		body {
+				background-color:
+								#8F1010;
+}
+		h1 {
+				text-align:
+							center;
+}
+	</style>
+</head>
+<body>
+<h1>HNG INTERNSHIP 4.0
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<?php echo date("h:i:sa");?>
+</h1>
+</body>
+</html>
